cc223c5828c8d2f1a2aff048c972e0c871f50a6e
Abdallah Mahameed <abdallah.mahameed@opensooq.com>
2024-09-15 12:20:37 +0300

    Initial Commit

---
+++ OSChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OSChat
+//
+//  Created by Abdallah Mahameed on 15/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
